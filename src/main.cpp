dd8f89a5e763261ee2f845f80f240bfd86481f21
a_m0d <damien.schoof@gmail.com>
2011-10-05 13:13:08 -0400

    Displaying a blank SFML 2.0 window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,28 @@
-int main (int argc, char* argv[]) {
-	return 0;
+#include <SFML/Window.hpp>
+
+int main () {
+    sf::Window app (sf::VideoMode (640, 480, 32), "SFML Window");
+
+    bool running = true;
+    while (running) {
+        sf::Event event;
+        while (app.PollEvent (event)) {
+            switch (event.Type) {
+                case sf::Event::Closed:
+                    running = false;
+                    break;
+                case sf::Event::KeyPressed:
+                    if (event.Key.Code == sf::Keyboard::Escape) {
+                        running = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        app.Display ();
+    }
+
+    return EXIT_SUCCESS;
 }
